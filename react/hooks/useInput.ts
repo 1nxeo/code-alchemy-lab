3901197aeceb8c77e8d8dd003ce35afd2ced051b
1nxeo <y.1nseooo@gmail.com>
2023-11-16 08:46:29 +0900

    HOOKS - useInput supports textarea and various types

--- react/hooks/useInput.ts
+++ react/hooks/useInput.ts
@@ -1,21 +1,36 @@
 import { useState } from "react";
 
-type InputType = string | number | boolean;
-
-interface InputValueProps {
-  initialValue: InputType | { [key: string]: InputType };
+interface InputValueProps<T> {
+  initialValue: T;
 }
 
-export const useInput = ({ initialValue }: InputValueProps) => {
-  const [inputValue, setInputValue] = useState(initialValue);
-  const isObject = typeof inputValue === "object";
+const isObject = (value: unknown): value is Record<string, unknown> => {
+  return typeof value === "object" && value !== null;
+};
 
-  const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value, checked, type } = event.target;
+export const useInput = <T>({ initialValue }: InputValueProps<T>) => {
+  const [inputValue, setInputValue] = useState<T>(initialValue);
+
+  const onChangeHandler = (
+    event:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    const { name, value, type } = event.target;
+
+    // input 요소의 경우 type이 checkbox인지 확인하여 checked 속성 가져오기
+    const checked =
+      type === "checkbox"
+        ? (event.target as HTMLInputElement).checked
+        : undefined;
 
     const newValue = type === "checkbox" ? checked : value;
 
-    setInputValue(isObject ? { ...inputValue, [name]: newValue } : newValue);
+    setInputValue((prev) =>
+      isObject(prev)
+        ? ({ ...inputValue, [name]: newValue } as T)
+        : (newValue as T)
+    );
   };
 
   return {
@@ -24,3 +39,17 @@ export const useInput = ({ initialValue }: InputValueProps) => {
     setInputValue,
   };
 };
+
+/**
+ * 사용법
+ * 
+ * interface UserInfo {
+ *    name:string;
+ *    age:number;
+ *    address:string;
+ * }
+ * 
+ * const { onChangeHandler, inputValue, setInputValue } = useInput<UserInfo>({
+    initialValue: '', // 초기값 설정
+  });
+ */
